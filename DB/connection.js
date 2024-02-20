4f6fbc1d908e16600b71e62b5663ec5a0da05de1
Mo7amed-Ali186 <m07amedali2345@gmail.com>
2024-02-20 13:01:46 +0200

    updated

--- DB/connection.js
+++ DB/connection.js
@@ -1,27 +1,10 @@
-// import  Mongoose from 'mongoose'
+import  Mongoose from 'mongoose'
 
-// export const connection  = async ()=>{
-//     return await Mongoose.connect(process.env.URI)
-//     .then((result)=>{
-//         console.log(" DB Connected Successfully on ..............")
-//     }).catch(error=>{
-//         console.log("Fail in Connection");
-//     })
-// }
-
-import Mongoose from 'mongoose';
-
-export const connection = async () => {
-    try {
-        await Mongoose.connect(process.env.URI, {
-            // bufferCommands: false, // Disable command buffering
-            bufferTimeoutMS: 0,    // Set buffer timeout to 0ms
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            // Other connection options...
-        });
-        console.log("DB Connected Successfully on ..............");
-    } catch (error) {
-        console.error("Failed to connect to the database:", error);
-    }
-};
+export const connection  = async ()=>{
+    return await Mongoose.connect(process.env.URI)
+    .then((result)=>{
+        console.log(" DB Connected Successfully on ..............")
+    }).catch(error=>{
+        console.log("Fail in Connection");
+    })
+}
